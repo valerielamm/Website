c3d43b8bca647de8519921758a9cd0b3151f87dc
Valerie <valerielamm.com@gmail.com>
2019-11-08 15:47:12 -0600

    Makes height responsive after image is loaded

--- lazyload.js
+++ lazyload.js
@@ -28,6 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         lazyImage.src = lazyImage.dataset.src;
                         lazyImage.srcset = lazyImage.dataset.srcset;
                         lazyImage.classList.remove("lazy");
+                        lazyImage.style.height = "auto";
 
                         lazyImages = lazyImages.filter(function (image) {
                             return image !== lazyImage;
