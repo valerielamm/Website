1d6ef94c01cebf0fec1605cf3481b9cc1a6fdaef
Valerie Lamm <36543470+valerielamm@users.noreply.github.com>
2023-07-12 15:21:57 -0500

    make slideshow.js better
    previously you had to insert an onload function in the body tag, add circles for each image, and tell the js function how many images there were along with what slide its on.
    
    make slideshow js make its own buttons based off the amount of images in div. it loads itself without body code in html.
    
    fixed some spacing and comments in files

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,39 +1,66 @@
-function slideShow(slide, numberOfSlides) {
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("w3-badge");
-    var buttonAll = document.getElementById("buttonAll");
-    buttonAll.style.display = "inline";
+function initializeSlideShow() {
+    // Count number of images
+    var imgCount = document.getElementById("imgCountParent").getElementsByTagName("img").length;
+    console.log(imgCount + "is the number of dots and slides in the slideshow");
 
-    // 22 is the number used for show all and collapse
-    if (slide == "22") {
+    // Creates and places the circle buttons - same number as imgs
+    var slideShowButtonsParent = document.getElementById("slideShowButtonsParent");
+    for (let i=1; i <= imgCount; i++) {
+        var newCircle = document.createElement('span');
+        newCircle.classList.add("w3-badge");
+        newCircle.setAttribute("onclick", "switchSlide(" + i + ")");
+        if(i==1){newCircle.className += " white-circle";}; // Makes first circle visually selected
+        slideShowButtonsParent.appendChild(newCircle);
+    };
+
+    // Displays first image
+    var slideImg = document.getElementsByClassName("mySlides");
+    slideImg[0].style.display = "block";
+} 
+
+initializeSlideShow();
+
+
+function switchSlide(slideCount){
+    var buttonAll = document.getElementById("buttonAll");
+    var imgCount = document.getElementById("imgCountParent").getElementsByTagName("img").length;
+    var slideImg = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("w3-badge");
+
+    // 21 is the slide number used to trigger 'show all' and 'collapse'
+    if (slideCount == "21") {
+        // Clicking the collapse button (goes to showing only slide 1)
         if (buttonAll.innerHTML == "Collapse") {
-            for (let i = 1; i < x.length; i++) {
-                x[i].style.display = "none";
+            for (let i = 1; i < imgCount; i++) {
+                slideImg[i].style.display = "none";
             }
-            for (let i = 0; i < dots.length; i++) {
+            for (let i = 1; i < dots.length; i++) { // Skips the first circle so it stays visually selected
                 dots[i].className = dots[i].className.replace(" white-circle", "");
             }
-            dots[0].className += " white-circle";
             buttonAll.innerHTML = "Show All Slides";
         } else {
-            for (let i = 0; i < numberOfSlides; i++) {
-                x[i].style.display = "block";
+            // Clicking the show all button
+            for (let i = 0; i < imgCount; i++) {
+                slideImg[i].style.display = "block";
             }
-            for (let i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" white-circle", ""); // remove existing before adding another
+            for (let i = 0; i < dots.length; i++) { // All circles are the visually selected when showing all slides
+                dots[i].className = dots[i].className.replace(" white-circle", ""); // Removes existing class before adding another to avoid duplicates
                 dots[i].className += " white-circle";
             }
             buttonAll.innerHTML = "Collapse";
         }
     } else {
+        // Reset incase all slides are open
         buttonAll.innerHTML = "Show All Slides";
-        for (let i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
+        for (let i = 0; i < imgCount; i++) {
+            slideImg[i].style.display = "none";
         }
         for (let i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" white-circle", "");
+            dots[i].className = dots[i].className.replace(" white-circle", ""); // Removes existing class before adding another to avoid duplicates
         }
-        x[slide - 1].style.display = "block";
-        dots[slide - 1].className += " white-circle";
+
+        // Selects correct img and circle
+        slideImg[slideCount - 1].style.display = "block";
+        dots[slideCount - 1].className += " white-circle";
     }
-}
+};
